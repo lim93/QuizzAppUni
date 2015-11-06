f652914ac29ee4c354cd62a6e0fd4f4212a8a0f5
Marc Westermann <westermann.m@gmail.com>
2015-11-06 13:15:30 +0100

    inserted other changes

--- app/src/main/java/quizzappuni/com/whs/quizzappuni/Activities/LearnMultiplechoice.java
+++ app/src/main/java/quizzappuni/com/whs/quizzappuni/Activities/LearnMultiplechoice.java
@@ -1,21 +1,23 @@
 package quizzappuni.com.whs.quizzappuni.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.widget.Button;
-import android.widget.ToggleButton;
-import android.content.Intent;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
 
 import quizzappuni.com.whs.quizzappuni.quizzappuni.R;
 
 public class LearnMultiplechoice extends AppCompatActivity {
 
-    private Button Antwort1;
-    private Button Antwort2;
-    private Button Antwort3;
-    private Button Antwort4;
+    public FloatingActionButton fabSend;
+    private ToggleButton Antwort1;
+    private ToggleButton Antwort2;
+    private ToggleButton Antwort3;
+    private ToggleButton Antwort4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,17 +27,18 @@ public class LearnMultiplechoice extends AppCompatActivity {
         //Ãœbergebene Fragen und Antworten entgegennehmen
         Intent start = getIntent();
 
-        Antwort1 = (Button)findViewById(R.id.Antwort1);
-
         // Erstellen der Toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
         setSupportActionBar(toolbar);
-
-        getSupportActionBar().setTitle("");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        fabSend = (FloatingActionButton) findViewById(R.id.fabSend);
+        fabSend.setVisibility(View.GONE);
 
         Antwort1 = (ToggleButton) findViewById(R.id.Antwort1);
 //        Antwort1.setTextOn("1.Antwort");
+
         Antwort2 = (ToggleButton) findViewById(R.id.Antwort2);
 //        Antwort2.setText("2.Antwort");
 //
@@ -45,15 +48,50 @@ public class LearnMultiplechoice extends AppCompatActivity {
         Antwort4 = (ToggleButton) findViewById(R.id.Antwort4);
 //        Antwort4.setText("4.Antwort");
 
+//        Listener für Button Änderung.
+        Antwort1.setOnCheckedChangeListener(changeChecker);
+        Antwort2.setOnCheckedChangeListener(changeChecker);
+        Antwort3.setOnCheckedChangeListener(changeChecker);
+        Antwort4.setOnCheckedChangeListener(changeChecker);
 
     }
 
-    /**
-     public void onBackPressed() {
-     // Call the MainActivity, if the back-button is pressed.
-     Intent i = new Intent(LearnMultiplechoice.this, MainActivity.class);
-     startActivity(i);
-     }
-     */
+    CompoundButton.OnCheckedChangeListener changeChecker = new CompoundButton.OnCheckedChangeListener() {
 
+//      Nur ein Button kann selected sein.
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            if (isChecked){
+                if (buttonView == Antwort1) {
+                    Antwort2.setChecked(false);
+                    Antwort3.setChecked(false);
+                    Antwort4.setChecked(false);
+                }
+                if (buttonView == Antwort2) {
+                    Antwort1.setChecked(false);
+                    Antwort3.setChecked(false);
+                    Antwort4.setChecked(false);
+                }
+                if (buttonView == Antwort3) {
+                    Antwort2.setChecked(false);
+                    Antwort1.setChecked(false);
+                    Antwort4.setChecked(false);
+                }
+                if (buttonView == Antwort4) {
+                    Antwort2.setChecked(false);
+                    Antwort3.setChecked(false);
+                    Antwort1.setChecked(false);
+                }
+
+//                FAB an
+                fabSend.setVisibility(View.VISIBLE);
+
+            }   else {
+//                FAB aus
+                fabSend.setVisibility(View.GONE);
+            }
+        }
+    };
 }
+
+
